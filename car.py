d53164aff3ee32e3f43b8e1f77663b9dcb455038
Subham Singh <geekysubham@gmail.com>
2023-10-21 04:18:25 +0530

    add new tires flow

--- car.py
+++ car.py
@@ -1,9 +1,10 @@
 from servicable import Servicable
 
 class Car(Servicable):
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery, tires):
         self.engine = engine
         self.battery = battery
+        self.tires = tires
 
     def needs_service(self):
-        return self.engine.needs_service() or self.battery.needs_service()
+        return self.engine.needs_service() or self.battery.needs_service() or self.tires.needs_service()
