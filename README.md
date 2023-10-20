835772fb5c0d484831d4f1fadb457a7351203131
Subham Singh <geekysubham@gmail.com>
2023-10-21 03:38:31 +0530

    refactor lyft project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lyft Repo
+This repo is refactored version of existing codebase which was earlier discontinued by lift backend engineer.
---
+++ car.py
@@ -0,0 +1,9 @@
+from servicable import Servicable
+
+class Car(Servicable):
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
+
+    def needs_service(self):
+        return self.engine.needs_service() or self.battery.needs_service()
